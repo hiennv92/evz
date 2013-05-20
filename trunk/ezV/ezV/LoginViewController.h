cfc7d2692b66996cfb2506efb40cb290c7bca7c2
nguyenviethien.1992@gmail.com <nguyenviethien.1992@gmail.com@2fd2788e-4c2f-bfe5-3f86-bba3b77f3fff>
2013-05-20 13:19:25 +0000

    Phan giao dien dang nhap cho ipad chua chinh xong

---
+++ trunk/ezV/ezV/LoginViewController.h
@@ -0,0 +1,20 @@
+//
+//  LoginViewController.h
+//  ezV
+//
+//  Created by papo on 5/18/13.
+//  Copyright (c) 2013 ezV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController <UITextFieldDelegate>{
+    IBOutlet UITextField* m_UserNameTextField;
+    IBOutlet UITextField* m_PasswordTextField;
+    IBOutlet UIButton*    m_LoginButton;
+    IBOutlet UIButton*    m_SignUpButton;
+}
+
+-(IBAction)signInButtonPressed:(id)sender;
+-(IBAction)signUpButtonPressed:(id)sender;
+@end
